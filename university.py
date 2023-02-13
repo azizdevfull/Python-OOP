5f723502a82c44fa64379f5eb518202608119088
Azizbek Isroilov <102867709+azizdevfull@users.noreply.github.com>
2023-02-13 10:01:06 +0000

    completed 3thrd exercise

--- university.py
+++ university.py
@@ -32,7 +32,41 @@ class Teacher(Staff):
     def more_info(self):
         print(f"University: {self.university} Staff Name: {self.first_name} Last Name: {self.last_name} Position: {self.position} Subject: {self.subject}")
   
-            
+class OtherStaff(Staff):
+    def __init__(self,university,first_name, last_name, age,position):
+        super().__init__(university,first_name, last_name, age)
+        self.position = position
+       
+    def more_info(self):
+        print(f"University: {self.university} Staff Name: {self.first_name} Last Name: {self.last_name} Position: {self.position}")
+class Object(University):
+    def __init__(self, university,name):
+        super().__init__(university)
+        self.name = name
+        
+    def more_info(self):
+        print(f"University: {self.university} Name: {self.name} ")
+
+class Computer(Object):
+    def __init__(self,university,name,soni, tizimi, holati):
+        super().__init__(university,name)
+        self.soni = soni
+        self.tizimi = tizimi
+        self.holati = holati
+        
+    def more_info(self):
+        print(f"University: {self.university} Name: {self.name} Soni: {self.soni} Tizimi: {self.tizimi} Holati: {self.holati}")
+
+class Mebel(Object):
+    def __init__(self,university,name,soni, turi, holati):
+        super().__init__(university,name)
+        self.soni = soni
+        self.turi = turi
+        self.holati = holati
+        
+    def more_info(self):
+        print(f"University: {self.university} Name: {self.name} Soni: {self.soni} Turi: {self.turi} Holati: {self.holati}")
+         
 # uni = University("Tatu")
 # uni.info()
 
@@ -42,5 +76,18 @@ class Teacher(Staff):
 # stud = Student("Tatu","Omon", "Omonov", 22, "A")
 # stud.more_info()
 
-stud = Teacher("Tatu","Omon", "Omonov", 22, "Star", "Sub")
-stud.more_info()
+# stud = Teacher("Tatu","Omon", "Omonov", 22, "Star", "Sub")
+# stud.more_info()
+
+# o_staff = OtherStaff("Tatu","Omon", "Omonov", 22, "Star")
+# o_staff.more_info()
+
+# obj = Object("Tatu","Omon")
+# obj.more_info()
+
+# comp = Computer("Tatu","Omon", 250, "Windows", "Yaxshi")
+# comp.more_info()
+
+meb = Mebel("Tatu","Omon", 250, "Krovat", "Yaxshi")
+meb.more_info()
+
